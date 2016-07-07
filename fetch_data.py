ea62f7a7f7690543bdc43422de964d69be63b367
Alexander Paterson <alex-m-paterson@hotmail.co.uk>
2016-07-07 11:37:55 +0100

    Updating version control via Pycharm: adding Scrapy changes

---
+++ fetch_data.py
@@ -0,0 +1,35 @@
+import sys
+import csv
+import scrapy
+from scrapy.http import FormRequest
+from scrapy.crawler import CrawlerProcess
+
+
+class MyItem(scrapy.Item):
+    test = scrapy.Field()
+
+
+class Spider(scrapy.Spider):
+    start_urls = [
+        "blah.com",
+    ]
+
+    def parse(self, response):
+        blahblah = MyItem()
+        # Some Code
+        yield blahblah
+
+
+class crawler:
+    def start(self):
+        process = CrawlerProcess({
+            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+            'LOG_LEVEL': 'INFO',
+            'FEED_FORMAT': 'csv',
+            'FEED_URI': 'Output.csv'
+        })
+        process.crawl(Spider)
+        process.start()
+
+app = crawler()
+app.start()
